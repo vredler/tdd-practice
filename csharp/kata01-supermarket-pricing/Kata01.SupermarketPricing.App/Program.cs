53658941016c1974db8b8b570a721b4be8eadf7c
Victor Redler <victor.redler@colligent.se>
2021-04-16 09:47:20 +0200

    kata01 init commit

---
+++ csharp/kata01-supermarket-pricing/Kata01.SupermarketPricing.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata01.SupermarketPricing.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
